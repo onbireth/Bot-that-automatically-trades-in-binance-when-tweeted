ea581b6d3e46d5d2700cd1e13532d9cbf2306098
onbir.eth <121510174+onbireth@users.noreply.github.com>
2023-01-01 19:56:07 +0300

    Add files via upload

---
+++ binance_trade.py
@@ -0,0 +1,64 @@
+import binance
+import api
+from pprint import pprint
+import time
+
+client = binance.Client(api.api_key, api.api_secret, testnet=True)
+
+
+def get_asset_balance(symbol):
+    try:
+        asset = client.get_asset_balance(asset=symbol)
+        asset_balance = asset["free"]
+        print(f"{symbol} balance: {asset_balance}")
+    except:
+        print("asset not found")
+
+
+def buy(symbol="ETHUSDT", quantity=10):
+    try:
+        client.order_market_buy(
+            symbol=symbol,
+            quantity=quantity)
+        buy_price = get_price(symbol)
+        print(f"purchase price: {buy_price}")
+        time.sleep(1.5)
+        print("purchase successful")
+        return float(buy_price)
+    except:
+        print("failed purchase")
+
+
+def sell(symbol="ETHUSDT", quantity=10):
+    try:
+        client.order_market_sell(
+            symbol=symbol,
+            quantity=quantity)
+        sale_price = get_price(symbol)
+        print(f"sale price: {sale_price}")
+        time.sleep(1.5)
+        print("sale successful")
+        return float(sale_price)
+    except:
+        print("failed sell")
+
+
+def get_price(symbol):
+    price = client.get_ticker(symbol=symbol)["lastPrice"]
+    return price
+
+
+def Calculate(sell, buy):
+    profit_loss = (sell - buy) * 10
+    time.sleep(1.5)
+    print(f"profit/loss: ${profit_loss}")
+    return
+
+
+def buy_sell():
+    buy_result = buy()
+    time.sleep(30)
+    sell_result = sell()
+    time.sleep(1)
+    Calculate(sell_result, buy_result)
+    get_asset_balance("usdt")
---
+++ twitter_check.py
@@ -0,0 +1,47 @@
+import tweepy
+import time
+import api
+from binance_bot import *
+
+
+auth = tweepy.OAuth1UserHandler(
+    api.Tapi_key, api.Tapi_secret, api.Tacces_key, api.Tacces_secret)
+
+api = tweepy.API(auth)
+
+search_words = ["ETH", "eth", "ethereum", "Ethereum", "ETHEREUM"]
+
+last_tweet = api.user_timeline(screen_name="kebir000", count=1)[0].text
+
+count = 0
+
+
+def check():
+    tweet = api.user_timeline(screen_name="kebir000", count=1)[0].text
+
+    global last_tweet
+    if tweet != last_tweet:
+        last_tweet = tweet
+        print(f"tweeted: {last_tweet}")
+        time.sleep(1)
+
+        print("**********")
+
+        for word in search_words:
+            if word in last_tweet:
+                print("word found, purchase in process")
+                buy_sell()
+                global count
+                count += 1
+        if count == 0:
+            print("word not found")
+
+    else:
+        print("not tweeted")
+
+
+while True:
+    check()
+    if count != 0:
+        break
+    time.sleep(2)
